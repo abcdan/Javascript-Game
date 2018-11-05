2deec0a94f87670b0a767fc993a87ca02de457f5
Like <dahnjohl@gmail.com>
2018-11-05 16:07:58 +0100

    :tada: init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Load needed libraries, be sure do do npm install before running this code
+const randomWords = require('random-words');
+const request = require('request');
+const colors = require('colors')
+
+var word = randomWords()
+console.log(`Picked word: ${word}`.yellow);
+    url = `https://www.npmjs.com/package/${word}` // This is the URL that the request will use
+    request(url, (err, res, body) => {
+        if (res.statusCode === 404) { // This che
+            console.log("Take a shot!".red) 
+        }else{
+            console.log(`You're lucky! The word isn't a NPM package!`.green)
+        }
+        })
